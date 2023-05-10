365c7149cd87e8eb79133b0efe1ec4899dccb104
Matheus <matheus_501@hotmail.com>
2023-05-10 07:27:30 -0300

    utilizando knex no projeto

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 
 const usersRouter = require('./users.routes');
 const productsRouter = require('./products.routes');
+
 const routes = Router();
 routes.use('/users', usersRouter);
 routes.get('/products', productsRouter);
