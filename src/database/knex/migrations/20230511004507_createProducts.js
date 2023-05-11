aa6a3e633db2ae81af3b840d40259ca6f4d156db
Matheus <matheus_501@hotmail.com>
2023-05-10 23:41:26 -0300

    criando tabela de produtos com o knex

---
+++ src/database/knex/migrations/20230511004507_createProducts.js
@@ -0,0 +1,13 @@
+exports.up = (knex) =>
+  knex.schema.createTable('products', (table) => {
+    table.increments('id');
+    table.text('img');
+    table.text('name');
+    table.text('type');
+    table.text('description');
+    table.integer('price');
+    table.timestamp('created_at').default(knex.fn.now());
+    table.timestamp('updated_at').default(knex.fn.now());
+  });
+
+exports.down = (knex) => knex.schema.dropTable('products');
